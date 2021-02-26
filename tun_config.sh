7e8019fa7126079d22b3a03916dd42fd9d3110ea
= <=>
2021-02-26 16:23:40 +0100

    Passing name, address and mask as arguments

--- tun_config.sh
+++ tun_config.sh
@@ -5,8 +5,24 @@ if [ "$(whoami)" != "root" ]; then
 	exit 1
 fi
 
-openvpn --mktun --dev tunX
+name=${name:-X}
+addr=${addr:-10.0.0.1}
+mask=${mask:-24}
 
-ip link set tunX up
 
-ip addr add 10.0.0.1/24 dev tunX
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+        # echo $1 $2 // Optional to see the parameter:value result
+   fi
+
+  shift
+done
+
+openvpn --mktun --dev tun$name
+
+ip link set tun$name up
+
+ip addr add $addr/$mask dev tun$name
